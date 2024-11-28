17dd8a9fd11f8f907abb885738077468a63d686e
JoaoVitorSDelfino <103132209+JoaoVitorSDelfino@users.noreply.github.com>
2024-11-28 10:16:12 -0300

    feat: controle de investimentos (adicionar, excluir, editar e vizualizar)

---
+++ src/controller/investmentControl.js
@@ -0,0 +1,46 @@
+const Investment = require('../models/Investment')
+
+module.exports = {
+    criar: async (dados) => {
+        console.log(dados)
+
+        const novoInvestimento = await Investment.create(dados)
+
+        return {status: true, mensagem: 'Sucesso ao criar Investment!', investment: novoInvestimento}
+    },
+
+    buscarPorId: async (id) => {
+        const Investimento = await Investment.findOne({where: {id: id}})
+
+        if (Investimento) {
+            return {status: true, mensagem: 'Sucesso ao buscar Investimento!', investment: Investimento}
+        } else {
+            return {status: false, mensagem: 'ERRO, Investimento não existe!'}
+        }   
+    },
+
+    alterar: async (id, novosDados) => {
+        await Investment.update(
+            novosDados, 
+            {where: {id: id}}
+        )
+
+        InvestimentoAtualizado = await Investment.findOne({
+            where: {id: id}
+        })    
+
+        return {status: true, mensagem: 'Sucesso ao alterar Investimento!', InvestimentoAtualizado: InvestimentoAtualizado}
+    },
+
+    deletar: async (id) => {
+        const Investment = await Investment.findOne({where: {id: id}})
+
+        await Investment.destroy({where: { id: id }})
+
+        return {status: true, mensagem: 'Sucesso ao deletar Investimento!', InvestimentoExcluido: Investment}
+    },
+
+    listar: async () => {
+        return await Investment.findAll()
+    },
+}
