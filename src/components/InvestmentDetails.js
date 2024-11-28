913959af808cc74d1e4e042f8841d63b4cf5882a
JoaoVitorSDelfino <103132209+JoaoVitorSDelfino@users.noreply.github.com>
2024-11-28 17:55:33 -0300

    fix: mostrar os valores do investimento em texto

--- src/components/InvestmentDetails.js
+++ src/components/InvestmentDetails.js
@@ -15,19 +15,62 @@ function InvestmentDetails() {
     }
   };
 
+  const organizeData = (data) => {
+    const organizedData = {};
+
+    Object.keys(data).forEach((year) => {
+      organizedData[year] = {};
+
+      Object.keys(data[year]).forEach((month) => {
+        const monthData = data[year][month][0];
+        console.log(data[year][month][0])
+        organizedData[year][month] = {
+          ValorFinal: monthData.ValorFinal,
+          Dias: Object.entries(monthData.ValorDias).map(([day, value]) => ({
+            Dia: day,
+            Valor: value,
+          })),
+        };
+      });
+    });
+
+    return organizedData;
+  };
+
   useEffect(() => {
     fetchInvestmentDetails();
   }, [id]);
 
   if (!investment) {
-    return <div>Carregando detalhes do investimento...</div>;
+    return <div>Carregando detalhes do investimento...</div>
   }
+
+    const investmentData = JSON.parse(investment.dados)
+    const structuredData = organizeData(investmentData)
 
   return (
     <div style={styles.container}>
       <h1>{investment.nome}</h1>
       <p>{investment.descricao}</p>
-      <pre style={styles.jsonPreview}>{JSON.stringify(investment.dados, null, 2)}</pre>
+
+      {Object.entries(structuredData).map(([year, months]) => (
+        <div key={year} style={styles.yearContainer}>
+          <h2>Ano: {year}</h2>
+          {Object.entries(months).map(([month, details]) => (
+            <div key={month} style={styles.monthContainer}>
+              <h3>Mês: {month}</h3>
+              <p>Valor Final: {details.ValorFinal}</p>
+              <ul>
+                {details.Dias.map((dayData) => (
+                  <li key={dayData.Dia}>
+                    Dia {dayData.Dia}: {dayData.Valor}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
@@ -41,12 +84,15 @@ const styles = {
     borderRadius: "10px",
     boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
   },
-  jsonPreview: {
-    backgroundColor: "#eee",
+  yearContainer: {
+    marginBottom: "20px",
+  },
+  monthContainer: {
+    marginBottom: "10px",
     padding: "10px",
+    backgroundColor: "#eee",
     borderRadius: "5px",
-    overflowX: "auto",
   },
 };
 
-export default InvestmentDetails;
+export default InvestmentDetails
